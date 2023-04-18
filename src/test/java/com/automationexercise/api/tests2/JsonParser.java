e34ff9445597ed2ea70dd56fdaa9f5cec9e4aa3c
Ezzerof <christian.bitca@gmail.com>
2023-04-18 20:02:38 +0100

    done tests for products list and brands list

--- src/test/java/com/automationexercise/api/tests2/JsonParser.java
+++ src/test/java/com/automationexercise/api/tests2/JsonParser.java
@@ -1,13 +1,15 @@
 package com.automationexercise.api.tests2;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+
 
 public class JsonParser {
 
@@ -15,7 +17,7 @@ public class JsonParser {
 
 
     public static Map<String, String> createMap(String file) {
-        Map<String, String> map =  new HashMap<>();
+        Map<String, String> map = new HashMap<>();
 
         String json;
         try {
@@ -31,15 +33,32 @@ public class JsonParser {
         return new String(Files.readAllBytes(Paths.get(file)));
     }
 
-    public static void writeResponseIntoJson(String json, String pathWhereToSave) {
-        try {
-            FileWriter file = new FileWriter(pathWhereToSave);
-            file.write(json);
-            file.flush();
-            file.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static Map<Integer,Map<String, Object>> parseJSON(String jsonStr) {
+        JSONObject jsonObj = new JSONObject(jsonStr);
+        JSONArray productsArr = jsonObj.getJSONArray("products");
+        Map<Integer, Map<String, Object>> productMap = new HashMap<Integer, Map<String, Object>>();
+
+        for(int i=0; i<productsArr.length(); i++) {
+            JSONObject productObj = productsArr.getJSONObject(i);
+            int id = productObj.getInt("id");
+            String name = productObj.getString("name");
+            String price = productObj.getString("price");
+            String brand = productObj.getString("brand");
+            JSONObject categoryObj = productObj.getJSONObject("category");
+            JSONObject usertypeObj = categoryObj.getJSONObject("usertype");
+            String usertype = usertypeObj.getString("usertype");
+            String category = categoryObj.getString("category");
+
+            Map<String, Object> productDetails = new HashMap<String, Object>();
+            productDetails.put("name", name);
+            productDetails.put("price", price);
+            productDetails.put("brand", brand);
+            productDetails.put("usertype", usertype);
+            productDetails.put("category", category);
+
+            productMap.put(id, productDetails);
         }
+        return productMap;
     }
 
 }
--- src/test/java/com/automationexercise/api/tests2/ProductsBrandsLists.java
+++ src/test/java/com/automationexercise/api/tests2/ProductsBrandsLists.java
@@ -1,45 +1,116 @@
 package com.automationexercise.api.tests2;
 
 import com.automationexercise.api.endpoints.Routes;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.*;
 
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 @TestClassOrder(ClassOrderer.OrderAnnotation.class)
 public class ProductsBrandsLists {
 
     static Response response;
-    static Map<String, String> items;
-    static String stringResponse;
-    static String pathToJson = "src/test/resources/ProductsList.json";
+    JsonPath expectedJsonPath;
+    static String pathToJson = "src/test/resources/ExpectedProductsList.json";
+
+    static void getResponse(String url) {
+        response = given()
+                .when()
+                .get(url)
+                .then()
+                .extract().response();
+    }
 
 
     @Order(1)
     @Nested
     @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
     class GetAllProductsList {
-        @Order(1)
-        @Test
-        @DisplayName("Get the products list")
-        void getTheProductsList() {
+        @TestFactory
+        @DisplayName("Validate each product")
+        List<DynamicTest> validateEachProduct() {
+            getResponse(Routes.getProducts_url);
 
-            response = given()
-                    .when()
-                    .get(Routes.getProducts_url)
-                    .then()
-                    .extract().response();
+            String expectedJson;
+            try {
+                expectedJson = new String(Files.readAllBytes(Paths.get(pathToJson)));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            expectedJsonPath = new JsonPath(expectedJson);
 
-            stringResponse = response.asString();
-            JsonParser.writeResponseIntoJson(stringResponse, pathToJson);
+            List<String> productIds = response.jsonPath().getList("products.id", String.class);
+            List<Integer> productIdInts = productIds.stream().map(Integer::parseInt).toList();
 
+            List<DynamicTest> dynamicTests = new ArrayList<>();
+            for (Integer id : productIdInts) {
+                dynamicTests.add(DynamicTest.dynamicTest("Validate product with ID " + id, () -> {
+                    assertThat(response.jsonPath().getString("products.find { it.id == " + id + "}.id"), equalTo(expectedJsonPath.getString("products.find { it.id == " + id + "}.id")));
+                    assertThat(response.jsonPath().getString("products.find { it.id == " + id + "}.name"), equalTo(expectedJsonPath.getString("products.find { it.id == " + id + "}.name")));
+                    assertThat(response.jsonPath().getString("products.find { it.id == " + id + "}.price"), equalTo(expectedJsonPath.getString("products.find { it.id == " + id + "}.price")));
+                    assertThat(response.jsonPath().getString("products.find { it.id == " + id + "}.brand"), equalTo(expectedJsonPath.getString("products.find { it.id == " + id + "}.brand")));
+                    assertThat(response.jsonPath().getString("products.find { it.id == " + id + "}.category.usertype.usertype"), equalTo(expectedJsonPath.getString("products.find { it.id == " + id + "}.category.usertype.usertype")));
+                    assertThat(response.jsonPath().getString("products.find { it.id == " + id + "}.category.category"), equalTo(expectedJsonPath.getString("products.find { it.id == " + id + "}.category.category")));
+                }));
+            }
+            return dynamicTests;
         }
 
+        @Order(2)
+        @Test
+        @DisplayName("Test response code")
+        void testRespondMessage() {
+            getResponse(Routes.getProducts_url);
+            assertThat(response.jsonPath().getString("responseCode"), equalTo("200"));
+        }
 
     }
 
+    @Order(3)
+    @Nested
+    class getAllBrandsList {
+
+        @Order(1)
+        @Test
+        @DisplayName("Get the brands list")
+        void getTheProductsList() {
+            getResponse(Routes.getBrands_url);
+
+            String expectedJson;
+            try {
+                expectedJson = new String(Files.readAllBytes(Paths.get("src/test/resources/ExpectedBrandsList.json")));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            JsonPath expectedJsonPath = new JsonPath(expectedJson);
+
+            List<String> brandIds = response.jsonPath().getList("brands.id", String.class);
+            List<Integer> brandIdInts = brandIds.stream().map(Integer::parseInt).toList();
+            for (Integer id : brandIdInts) {
+                System.out.println("Validating brand with ID " + id);
+
+                assertThat(response.jsonPath().getString("brands.find { it.id == " + id + "}.id"), equalTo(expectedJsonPath.getString("brands.find { it.id == " + id + "}.id")));
+                assertThat(response.jsonPath().getString("brands.find { it.id == " + id + "}.brand"), equalTo(expectedJsonPath.getString("brands.find { it.id == " + id + "}.brand")));
+            }
+        }
+
+        @Order(2)
+        @Test
+        @DisplayName("Test response code")
+        void testRespondMessage() {
+            assertThat(response.jsonPath().getString("responseCode"), equalTo("200"));
+        }
+
+    }
 }
+
+
